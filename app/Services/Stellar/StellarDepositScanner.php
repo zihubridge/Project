f9e9e8a4e1497e67baa9fe9460a1a35dbd71e015
Zainh332 <zain_h332@hotmail.com>
2026-01-21 21:07:44 +0500

    url udpated

--- app/Services/Stellar/StellarDepositScanner.php
+++ app/Services/Stellar/StellarDepositScanner.php
@@ -11,7 +11,7 @@ class StellarDepositScanner
     {
         $address = $deposit->deposit_address;
 
-        $url = rtrim(env('STELLAR_PUBLIC_ADDRESS'), '/')
+        $url = rtrim(env('STELLAR_HORIZON_MAINNET'), '/')
             . "/accounts/{$address}/payments?order=desc&limit=100";
 
         $res = Http::timeout(15)->get($url);
@@ -32,7 +32,7 @@ class StellarDepositScanner
             $txHash = $p['transaction_hash'];
 
             $tx = Http::timeout(15)->get(
-                rtrim(env('STELLAR_PUBLIC_ADDRESS'), '/') . "/transactions/{$txHash}"
+                rtrim(env('STELLAR_HORIZON_MAINNET'), '/') . "/transactions/{$txHash}"
             )->json();
 
             if (($tx['memo_type'] ?? null) !== 'id') continue;
