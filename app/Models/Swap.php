dd264309da7ad3194cc692f1364db9646673123f
Zainh332 <zain_h332@hotmail.com>
2026-01-26 18:17:45 +0500

    relations added

--- app/Models/Swap.php
+++ app/Models/Swap.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Swap extends Model
 {
     protected $guarded = [];
+
+    public function fromBlockchain()
+    {
+        return $this->belongsTo(Blockchain::class, 'from_blockchain_id');
+    }
+
+    public function toBlockchain()
+    {
+        return $this->belongsTo(Blockchain::class, 'to_blockchain_id');
+    }
+
+    public function deposit()
+    {
+        return $this->hasOne(SwapDeposit::class);
+    }
 }
--- app/Models/SwapDeposit.php
+++ app/Models/SwapDeposit.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class SwapDeposit extends Model
 {
     protected $guarded = [];
+
+    public function swap()
+    {
+        return $this->belongsTo(Swap::class);
+    }
+
+    public function expectedToken()
+    {
+        return $this->belongsTo(Token::class, 'expected_token_id');
+    }
 }
