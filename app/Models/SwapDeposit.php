7d2a0ca9bd561fd04bd9fdf3f8c183be68c8cbfd
Zainh332 <zain_h332@hotmail.com>
2026-03-03 21:40:03 +0500

    fixed

--- app/Models/SwapDeposit.php
+++ app/Models/SwapDeposit.php
@@ -7,8 +7,18 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SwapDeposit extends Model
 {
-    protected $guarded = [];
     use SoftDeletes;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'expires_at'  => 'datetime',
+        'received_at' => 'datetime',
+        'refunded_at' => 'datetime',
+        'deleted_at'  => 'datetime',
+        'created_at'  => 'datetime',
+        'updated_at'  => 'datetime',
+    ];
 
     public function swap()
     {
