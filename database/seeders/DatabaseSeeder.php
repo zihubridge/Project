6ef4c5a42a91fe6ddd0b7384c09d518d45d7dac2
Zainh332 <zain_h332@hotmail.com>
2026-03-16 22:07:42 +0500

    db seeder changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(BlockchainSeeder::class);
+        $this->call(ChainStateKeySeeder::class);
+        $this->call(DepositStateSeeder::class);
+        $this->call(InternalSwapStateSeeder::class);
+        $this->call(PlatformWalletsSeeder::class);
+        $this->call(SwapEventTypeSeeder::class);
+        $this->call(SwapExchangeStateSeeder::class);
+        $this->call(SwapPayoutStateSeeder::class);
+        $this->call(SwapStatesSeeder::class);
+        $this->call(TokenSeeder::class);
     }
 }
