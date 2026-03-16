6e42edbe41f91fb59d3ff6dd870bc4f23e41ebd7
Zainh332 <zain_h332@hotmail.com>
2026-03-16 22:00:20 +0500

    bug fixed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -14,6 +15,7 @@ class UserFactory extends Factory
     /**
      * The current password being used by the factory.
      */
+    protected $model = User::class;
     protected static ?string $password;
 
     /**
@@ -24,8 +26,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
