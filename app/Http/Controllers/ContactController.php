1a35d2e4757212ea09cd4d98daf8d596a9c79912
Zainh332 <zain_h332@hotmail.com>
2026-03-27 23:40:41 +0500

    try catch added

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ContactFormMail;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class ContactController extends Controller
 {
@@ -16,8 +18,18 @@ class ContactController extends Controller
             'message' => 'required|string|max:5000',
         ]);
 
-        Mail::to('support@zihubridge.com')->send(new ContactFormMail($validated));
+        try {
+            Mail::to('support@zihubridge.com')
+                ->send(new ContactFormMail($validated));
 
-        return back()->with('success', 'Your message has been sent! We will get back to you within 24 hours.');
+            return back()->with('success', 'Your message has been sent! We will get back to you within 24 hours.');
+
+        } catch (Exception $e) {
+
+            // Log the real error (don’t hide it like amateurs)
+            Log::error('Mail sending failed: ' . $e->getMessage());
+
+            return back()->with('error', 'Failed to send message. Please try again later.');
+        }
     }
 }
