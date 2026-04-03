4434749110edb088c2d19f038b8bb160a046e1e0
Zainh332 <zain_h332@hotmail.com>
2026-04-04 00:19:42 +0500

    build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
     plugins: [
         laravel({
